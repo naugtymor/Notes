49c4be8be86b3030f37f7775a38c4f6502aee621
naughtymor <znaknikita.it@gmail.com>
2023-03-20 18:34:39 +0300

    feat: store

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import MainBlock from "./content/MainBlock/MainBlock";
+
+function App() {
+
+  return (
+    <div className="App">
+      <MainBlock/>
+    </div>
+  );
+}
+
+export default App;
